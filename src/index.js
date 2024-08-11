035992951c20a155eb83472842cca125188336d3
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-11 10:28:58 -0300

    update and import code to display flat information

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Hello from './components/hello';
+import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Hello name="James" lastName="Batata" />
+    <App />
   </React.StrictMode>
 );
 
