5ecb36302ddb2fd9b723aa4017c06b6def4ae873
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-11 20:59:18 -0300

    add key to Marker

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component{
             zoom={11}
           >
           {this.state.flats.map((flat) => {
-              return <Marker lat={flat.lat} lng={flat.lng} text={flat.price} />;
+              return <Marker key={flat.name} lat={flat.lat} lng={flat.lng} text={flat.price} />;
             })}
           </GoogleMapReact>
 
