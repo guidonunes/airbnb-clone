d50fa1dc7d6964bf5c9512c3d8b93c0a6a1a0cde
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-11 10:02:50 -0300

    add flat component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import Hello from './components/hello';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello name="Boris" />
+    {/* <App /> */}
+    <Hello name="James" lastName="Batata" />
   </React.StrictMode>
 );
 
