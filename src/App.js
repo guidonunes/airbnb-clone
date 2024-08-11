a2a2faf94f7e527d7a0ce842ffed2c0b0944077e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-11 19:13:27 -0300

    implement extends method on class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React, { Component } from 'react';
 import './App.css';
 import Flat from './components/flat';
 
-class App {
+class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
