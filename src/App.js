ef11fe51fa3482d2e43b259eefb11db2f87cdcf5
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-11 19:08:14 -0300

    call constructor function to implement state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import './App.css';
 import Flat from './components/flat';
 
-function App() {
-  const flat = {
-    "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-    "price": 164,
-    "priceCurrency": "EUR",
-    "lat": 48.884211,
-    "lng": 2.34689
-  };
+class App {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
 
-  const flats = [flat, flat];
-
-  return (
-    <div className="app">
-      <div className="main">
-        <div className="search">
+  render() {
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />;
+            })}
+          </div>
         </div>
-        <div className="flats">
-          {flats.map(function(flat) {
-            return <Flat flat={flat} />;
-          })}
+        <div className="map">
         </div>
       </div>
-      <div className="map">
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
