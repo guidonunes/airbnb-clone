a4e54d04d97a3047e5e428745df967937a1a6f56
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-11 10:22:07 -0300

    add variable to store background image style

--- src/components/flat.js
+++ src/components/flat.js
@@ -6,9 +6,13 @@ class Flat extends React.Component {
   render() {
     const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
 
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    };
+
     return (
       <div className="flat">
-        <div className="flat-picture"></div>
+        <div className="flat-picture" style={style}></div>
         <div className="flat-title">
           {title}
         </div>
