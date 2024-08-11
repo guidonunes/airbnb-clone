a4867fce8f772af612540100cf11e433d8a246db
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-11 10:15:43 -0300

    add const variable for title

--- src/components/flat.js
+++ src/components/flat.js
@@ -4,10 +4,14 @@ import "./flat.css";
 
 class Flat extends React.Component {
   render() {
+    const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
+
     return (
       <div className="flat">
         <div className="flat-picture"></div>
-        <div className="flat-title"></div>
+        <div className="flat-title">
+          {title}
+        </div>
       </div>
     );
   }
