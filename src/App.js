b909a95e22f93632e13c5dd19ff7b7d1e26e4358
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-11 21:44:55 -0300

    add selected props to Marker

--- src/App.js
+++ src/App.js
@@ -61,7 +61,12 @@ class App extends Component{
             zoom={11}
           >
           {this.state.flats.map((flat) => {
-              return <Marker key={flat.name} lat={flat.lat} lng={flat.lng} text={flat.price} />;
+              return <Marker
+                key={flat.name}
+                lat={flat.lat}
+                lng={flat.lng}
+                text={flat.price}
+                selected={flat === this.state.selectedFlat}/>;
             })}
           </GoogleMapReact>
 
